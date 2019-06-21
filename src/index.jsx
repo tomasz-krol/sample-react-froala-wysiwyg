cdfd9d3cc8ac1e1fc30c7adbc5fbb77290d6d3f5
Navneet Kumar <navneet.kumar@accoliteindia.com>
2019-06-21 13:39:30 +0530

    reprod issue

--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,45 @@
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 import 'froala-editor/css/froala_style.css';
 import FroalaEditor from 'react-froala-wysiwyg';
-import 'froala-editor/js/plugins.pkgd.min.js';
+
+import 'react-froala-wysiwyg/node_modules/froala-editor/js/plugins.pkgd.min.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
   config = {
-    // toolbarButtons:[]
-   }
+    attribution: false,
+    placeholderText: 'Write Your Content Here*',
+    charCounterCount: false,
+    toolbarButtons:
+    {
+      'moreText': {
+        'buttons': ['fullscreen', 'bold', 'italic', 'underline', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass']
+      },
+      'moreParagraph': {
+        'buttons': ['insertLink', 'insertTable', 'formatOL', 'formatULSimple', 'formatUL', 'alignLeft', 'alignCenter', 'alignRight', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote']
+      },
+    }
+  }
+  handleModelChange(htmlCode, type) {
+    console.log(htmlCode, type)
+  }
   render() {
     return <div>
-      <FroalaEditor tag='textarea' config={this.config} />
+      <FroalaEditor
+        tag='textarea'
+        config={this.config}
+        model={this.state.html || ""}
+        onModelChange={(htmlCode) => {
+          this.handleModelChange(htmlCode, "emailBody");
+        }}
+      />
     </div>
   }
 }
